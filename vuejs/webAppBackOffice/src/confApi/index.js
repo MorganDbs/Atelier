1959e19e4b5f948c120786d0f0d2230767676468
Matmat Myriam <matmat.myriam@gmail.com>
2018-02-06 09:28:01 +0100

    mise en place sign in, sihn up et logout

---
+++ vuejs/webAppBackOffice/src/confApi/index.js
@@ -0,0 +1,10 @@
+/**import axios from 'axios'
+
+const confApi = axios.create({
+  baseURL: 'mon localhost',
+  headers: {
+    'Authorization' : 'Token token=............'
+  }
+})
+export default confApi
+*/
