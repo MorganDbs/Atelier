4b8c5ce3c51321e39c7a6effc63e00777124e042
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2018-02-06 15:26:05 +0100

    Correction CORS

--- jee/src/main/java/org/lpro/provider/cors/CORSResponseFilter.java
+++ jee/src/main/java/org/lpro/provider/cors/CORSResponseFilter.java
@@ -21,6 +21,8 @@ public class CORSResponseFilter implements ContainerResponseFilter {
         responseCtx.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseCtx.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseCtx.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE ");
+        responseCtx.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, Origin, Accept");
+        responseCtx.getHeaders().add("Access-Control-Allow-Credentials", "true");
     }
 
 }
